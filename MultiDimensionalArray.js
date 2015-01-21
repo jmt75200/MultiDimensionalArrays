14f11b0f75b04a6bc3558dc28a8516f23bdadce2
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-21 10:03:57 -1000

    adding generator_1d and generator_2d to test-generator. test passes.

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,1 +1,19 @@
+module.exports = {
 
+  generator_1d : function (){
+    return [ 0,1,1 ];
+
+
+  },
+
+  generator_2d : function (){
+    return [ 0,1,1,0 ];
+
+  },
+
+  generator_3d : function (){
+
+  }
+
+
+};
--- test/test-generator.js
+++ test/test-generator.js
@@ -1,1 +1,31 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var mda = require('../MultiDimensionalArray.js');
+ 
+describe("mda", function() {
+   
+  describe('generator_1d', function() {
+    
+    it('Returns an array with 3 random boolean values', function() {
+      var result = mda.generator_1d();
+      
+      expect( result ).to.be.a('array');
+      result.should.have.length(3);
+    
+    });
+  
+  }); //end of first describe
 
+  describe("generator_2d", function() {
+    
+    it("Return a single array with 4 random boolean values", function() {
+      var result = mda.generator_2d();
+
+      expect( result ).to.be.a('array');
+      result.should.have.length(4);
+    });
+
+  });
+
+});
